8f2332ccf66f5081bc204d52b40016a8f1e69c68
NareshKumar87-byte <140261152+Nareshkumar-S-lap@users.noreply.github.com>
2025-09-29 23:16:24 +0530

    Ui readme updated and added health check route

--- shop-backend/src/routes/routes.ts
+++ shop-backend/src/routes/routes.ts
@@ -1,5 +1,6 @@
 import { assembleRoutes } from '@common/route/assembleRoutes';
+import HealthRoute from '@modules/health/healthRoute';
 import ShopRoute from '@modules/shop/shopRoute';
-const routes = assembleRoutes(ShopRoute);
+const routes = assembleRoutes(HealthRoute, ShopRoute);
 
 export default routes;
